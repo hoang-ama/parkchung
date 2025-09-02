53791114a76de79f797ddeee2fc0f0714e578708
Minh Hoang <minhhh@Minhs-MacBook-Air.local>
2025-09-02 21:32:16 +0700

    update home page

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const googleSheetScriptURL = 'https://script.google.com/macros/s/AKfycbw3v7HA2uQ5anB9WByDtgZe2fa7cRm3WGG5ZFmblTwd8MyHg4nv5u7POLVZ4ZrdGcMLdg/exec'; 
+
 document.getElementById('searchForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const location = document.getElementById('location').value.trim();
@@ -18,4 +20,32 @@ document.getElementById('searchForm').addEventListener('submit', function(event)
 
     alert(`Searching parking spots at "${location}" from ${startDate} ${startTime} to ${endDate} ${endTime}.`);
     // TODO: Redirect or make API call with search parameters
+        const subscribeForm = document.forms['email-subscription-form'];
+    if (subscribeForm) {
+        subscribeForm.addEventListener('submit', e => { 
+            e.preventDefault();
+            const emailInput = subscribeForm.querySelector('#subscriber-email');
+            const submitButton = subscribeForm.querySelector('.subscribe-button');
+            if (emailInput) {
+                const email = emailInput.value.trim();
+                if (validateEmail(email)) { 
+                    if(submitButton) { submitButton.disabled = true; submitButton.textContent = "Subscribing..."; }
+                    sendSubscriptionEmailToGoogleSheet(email, subscribeForm, submitButton);
+                } else { alert('Please enter a valid email address (e.g., example@email.com).'); }
+            }
+        });
+    }
+
+     function sendSubscriptionEmailToGoogleSheet(email, form, button) { 
+        const data = new FormData(); 
+        data.append('timestamp', new Date().toISOString()); data.append('email', email); data.append('sheetName', 'Email'); 
+        fetch(googleSheetScriptURL, { method: 'POST', body: data }) 
+            .then(response => response.text()) 
+            .then(textResponse => {
+                console.log('Subscription API Response:', textResponse); alert('Thank you for subscribing!'); if(form) form.reset();
+            })
+            .catch(error => { console.error('Subscription Error!', error.message); alert('An error occurred. Please try again.'); })
+            .finally(() => { if(button) { button.disabled = false; button.textContent = "Sign-up"; }});
+    }
+    
 });
